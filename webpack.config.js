2a66c3e0572361695c4d5303fe21bed7026e6598
Marcos Iglesias <miglesias@eventbrite.com>
2016-02-25 15:58:31 -0800

    Basic dev and build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,150 @@
 var webpack = require('webpack'),
     path = require('path'),
+
+    UglifyJsPlugin = webpack.optimize.UglifyJsPlugin,
+
+    env = process.env.WEBPACK_ENV, // dev | build
+    isProduction = env === 'prod',
+    isDevelopment = env === 'dev',
+
     chartModulesPath = path.resolve('./src/charts'),
-    fixturesPath = path.resolve('./test/fixtures');
+    fixturesPath = path.resolve('./test/fixtures'),
+    vendorsPath = path.resolve('./node_modules'),
 
-var config = {
-    module: {
+    projectName = 'britecharts',
 
-        preLoaders: [
-            {
-                test: /\.js$/,
-                include: /src/,
-                exclude: /(bower_components|node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                },
-            },
-            {
-                test: /\.js?$/,
-                include: /src/,
-                exclude: /(node_modules|bower_components|__tests__)/,
-                loader: 'babel-istanbul',
-                query: {
-                    cacheDirectory: true,
-                },
+    plugins = [],
+    outputFile,
+    config;
+
+
+// Set up minification for production
+if (isProduction) {
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    outputFile = projectName + '.min.js';
+} else {
+    outputFile = projectName + '.js';
+}
+
+config = {
+
+    dev: {
+        // allows webpack to determine where your app begins execution, and it creates chunks out of it
+        entry:  {
+            'bar': './src/charts/bar.js',
+            'donut': './src/charts/donut.js',
+            'legend': './src/charts/legend.js',
+            'line': './src/charts/line.js',
+            'tooltip': './src/charts/tooltip.js'
+        },
+
+        devtool: 'source-map',
+
+        // tells webpack what to name your files after the build process, and where to place them
+        output: {
+            path:     'dist/umd',
+            filename: '[name].js',
+            libraryTarget: 'umd'
+        },
+
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel',
+                    exclude: /(node_modules)/
+                }
+            ],
+
+            // Tell Webpack not to parse certain modules.
+            noParse: [
+                new RegExp(vendorsPath + '/d3/d3.js')
+            ]
+        },
+
+        resolve: {
+            alias: {
+                d3: vendorsPath + '/d3'
             }
-        ],
+        },
 
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /(node_modules)/
-            }
-        ]
+        plugins: plugins
     },
-    plugins: [
-    ],
-    resolve: {
-        root: [chartModulesPath, fixturesPath]
+
+    test: {
+        resolve: {
+            root: [chartModulesPath, fixturesPath],
+            alias: {
+                d3: vendorsPath + '/d3'
+            }
+        },
+        module: {
+            preLoaders: [
+                {
+                    test: /\.js$/,
+                    include: /src/,
+                    exclude: /(bower_components|node_modules)/,
+                    loader: 'babel',
+                    query: {
+                        cacheDirectory: true,
+                    },
+                },
+                {
+                    test: /\.js?$/,
+                    include: /src/,
+                    exclude: /(node_modules|bower_components|__tests__)/,
+                    loader: 'babel-istanbul',
+                    query: {
+                        cacheDirectory: true,
+                    },
+                }
+            ],
+
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel',
+                    exclude: /(node_modules)/
+                }
+            ]
+        },
+        plugins: plugins
+    },
+
+    // TODO: Check if this is actually right
+    prod: {
+        entry:  './src/charts/bar.js',
+        devtool: 'source-map',
+        resolve: {
+            alias: {
+                d3: vendorsPath + '/d3'
+            }
+        },
+        output: {
+            path:     'dist/bundled',
+            filename: outputFile,
+            libraryTarget: 'umd'
+        },
+        module: {
+
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel',
+                    exclude: /(node_modules)/
+                }
+            ],
+
+            // Tell Webpack not to parse certain modules.
+            noParse: [
+                new RegExp(vendorsPath + '/d3/d3.js')
+            ]
+        },
+        plugins: plugins
     }
 };
 
-module.exports = config;
+if (isDevelopment) {
+}
+
+module.exports = config[env];
