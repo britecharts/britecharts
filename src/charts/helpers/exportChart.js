24bd4542b789986007c07f89be80cbf909d0009c
Ryan Wholey <rwholey-eb@eventbrite.com>
2016-04-26 11:14:03 -0700

    cleanup & update params for jsdoc

--- src/charts/helpers/exportChart.js
+++ src/charts/helpers/exportChart.js
@@ -19,34 +19,16 @@ define(function(require) {
     };
 
     /**
-     * Main function to be used as a method by chart
-     *     instances to export charts to png
-     * @param  {array | d3 svg} svgs [pass in both chart & legend as array
-     *     or just chart as svg or in array]
+     * Main function to be used as a method by chart instances to export charts to png
+     * @param  {array} svgs (or an svg element) pass in both chart & legend as array or just chart as svg or in array
      * @param  {string} filename [download to be called <filename>.png]
      */
-    function exportChart(svgs, filename) {
-        let d3svg,
-            legend,
-            html,
+    function exportChart(d3svg, filename) {
+        let html,
             img,
-            legendHtml,
-            legendImg,
             canvas,
             canvasWidth = this.width(),
             canvasHeight = this.height();
-
-        if (svgs.data) {
-            d3svg = svgs;
-        } else {
-            [d3svg,legend] = svgs;
-        }
-        //legend functionality not complete
-        if (legend) {
-            canvasHeight += legend.height();
-            legendHtml = convertSvgToHtml(legend.getD3SVG());
-            legendImg = createImage(legendHtml);
-        }
 
         html = convertSvgToHtml(d3svg);
 
@@ -54,21 +36,13 @@ define(function(require) {
 
         img = createImage(html);
 
-        if (legendImg) {
-            img.onload = function(e) {
-                e.preventDefault();
-                drawImageOnCanvas(img, canvas);
-                legendImg.onload = handleImageLoad.bind(this, legendImg, canvas);
-            };
-        } else {
-            img.onload = handleImageLoad.bind(img, canvas, filename);
-        }
+        img.onload = handleImageLoad.bind(img, canvas, filename);
     }
 
     /**
      * takes d3 svg el, adds proper svg tags, adds inline styles
      * from stylesheets, adds white background and returns string
-     * @param  {d3 svg el} d3svg
+     * @param  {object} d3svg TYPE d3 svg element
      * @return {string} string of passed d3
      */
     function convertSvgToHtml (d3svg) {
@@ -84,7 +58,7 @@ define(function(require) {
      * Create Canvas
      * @param  {number} width
      * @param  {number} height
-     * @return {el <canvas>}
+     * @return {object} TYPE canvas element
      */
     function createCanvas(width, height) {
         let canvas = document.createElement('canvas');
@@ -97,7 +71,7 @@ define(function(require) {
     /**
      * Create Image
      * @param  {string} svgHtml string representation of svg el
-     * @return {el <img>}  src points at svg
+     * @return {object}  TYPE element <img>, src points at svg
      */
     function createImage(svgHtml) {
         let img = new Image();
@@ -108,8 +82,8 @@ define(function(require) {
 
     /**
      * Draws image on canvas
-     * @param  {el <img>} image to draw
-     * @param  {el <canvas>} canvas to be drawn on
+     * @param  {object} image TYPE:el <img>, to be drawn
+     * @param  {object} canvas TYPE: el <canvas>, to draw on
      */
     function drawImageOnCanvas(image, canvas) {
         canvas.getContext('2d').drawImage(image, 0, 0);
@@ -118,9 +92,9 @@ define(function(require) {
     /**
      * Triggers browser to download image, convert canvas to url,
      * point <a> at it and trigger click
-     * @param  {el <canvas>} canvas
-     * @param  {String} filename
-     * @param  {String} extensionType
+     * @param  {object} canvas TYPE: el <canvas>
+     * @param  {string} filename
+     * @param  {string} extensionType
      */
     function downloadCanvas(canvas, filename='britechart.png', extensionType='image/png') {
         let url = canvas.toDataURL(extensionType);
@@ -130,9 +104,9 @@ define(function(require) {
 
     /**
      * Handles on load event fired by img.onload, this=img
-     * @param  {[type]} canvas
-     * @param  {[type]} filename
-     * @param  {[type]} e
+     * @param  {object} canvas TYPE: el <canvas>
+     * @param  {string} filename
+     * @param  {object} e
      */
     function handleImageLoad(canvas, filename, e) {
         e.preventDefault();
