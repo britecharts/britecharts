5f548b0542a4435f5a33e126f42b136c777cf158
Marcos Iglesias <golodhros@gmail.com>
2016-07-17 20:48:58 +0100

    Using bar data builder and json

--- demos/demo-bar.js
+++ demos/demo-bar.js
@@ -1,118 +1,17 @@
 'use strict';
 
 var d3 = require('d3'),
-    bar = require('./../src/charts/bar');
+    bar = require('./../src/charts/bar'),
+    dataBuilder = require('./../test/fixtures/barChartDataBuilder');
 
 
 function createBarChart() {
     var barChart = bar(),
-        dataset = [
-            {
-                letter: 'A',
-                frequency: .08167
-            },
-            {
-                letter: 'B',
-                frequency: .01492
-            },
-            {
-                letter: 'C',
-                frequency: .02782
-            },
-            {
-                letter: 'D',
-                frequency: .04253
-            },
-            {
-                letter: 'E',
-                frequency: .12702
-            },
-            {
-                letter: 'F',
-                frequency: .02288
-            },
-            {
-                letter: 'G',
-                frequency: .02015
-            },
-            {
-                letter: 'H',
-                frequency: .06094
-            },
-            {
-                letter: 'I',
-                frequency: .06966
-            },
-            {
-                letter: 'J',
-                frequency: .00153
-            },
-            {
-                letter: 'K',
-                frequency: .00772
-            },
-            {
-                letter: 'L',
-                frequency: .04025
-            },
-            {
-                letter: 'M',
-                frequency: .02406
-            },
-            {
-                letter: 'N',
-                frequency: .06749
-            },
-            {
-                letter: 'O',
-                frequency: .07507
-            },
-            {
-                letter: 'P',
-                frequency: .01929
-            },
-            {
-                letter: 'Q',
-                frequency: .00095
-            },
-            {
-                letter: 'R',
-                frequency: .05987
-            },
-            {
-                letter: 'S',
-                frequency: .06327
-            },
-            {
-                letter: 'T',
-                frequency: .09056
-            },
-            {
-                letter: 'U',
-                frequency: .02758
-            },
-            {
-                letter: 'V',
-                frequency: .00978
-            },
-            {
-                letter: 'W',
-                frequency: .02360
-            },
-            {
-                letter: 'X',
-                frequency: .00150
-            },
-            {
-                letter: 'Y',
-                frequency: .01974
-            },
-            {
-                letter: 'Z',
-                frequency: .00074
-            }
-        ],
-        barContainer = d3.select('.js-bar-chart-container');
+        testDataSet = new dataBuilder.BarDataBuilder(),
+        barContainer = d3.select('.js-bar-chart-container'),
+        dataset;
+
+    dataset = testDataSet.withLettersFrequency().build();
 
     barChart
         .width(500)
---
+++ test/fixtures/barChartDataBuilder.js
@@ -0,0 +1,41 @@
+define(function(require) {
+    'use strict';
+
+    var _ = require('underscore'),
+
+        jsonLetters = require('json!../json/barDataLetters.json');
+
+
+    function BarDataBuilder(config){
+        this.Klass = BarDataBuilder;
+
+        this.config = _.defaults({}, config);
+
+        this.withLettersFrequency = function(){
+            var attributes = _.extend({}, this.config, jsonLetters);
+
+            return new this.Klass(attributes);
+        };
+
+        /**
+         * Sets the path for fetching the data
+         * @param  {String} path Desired path for test data
+         * @return {BarDataBuilder}      Builder object
+         */
+        this.withPath = function(path){
+            var attributes = _.extend({}, this.config, {
+                jsonURL: path
+            });
+
+            return new this.Klass(attributes);
+        };
+
+        this.build = function() {
+            return this.config.data;
+        };
+    }
+
+    return {
+        BarDataBuilder: BarDataBuilder
+    };
+});
