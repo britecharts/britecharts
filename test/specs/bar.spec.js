4b648d2555da971fd13d518f6241338efd17ddb3
Marcos Iglesias <golodhros@gmail.com>
2016-07-23 10:49:47 -0700

    Adding gridlines to the bar chart and updating tests and styles

--- test/specs/bar.spec.js
+++ test/specs/bar.spec.js
@@ -1,116 +1,17 @@
-define(['jquery', 'd3', 'bar'], function($, d3, chart) {
+define(['jquery', 'd3', 'bar', 'barChartDataBuilder'], function($, d3, chart, dataBuilder) {
     'use strict';
 
     describe('Reusable Bar Chart Test Suite', () => {
         let barChart, dataset, containerFixture, f;
 
+        function aTestDataSet() {
+            return new dataBuilder.BarDataBuilder();
+        }
+
         beforeEach(() => {
-            dataset = [
-                {
-                    letter: 'A',
-                    frequency: .08167
-                },
-                {
-                    letter: 'B',
-                    frequency: .01492
-                },
-                {
-                    letter: 'C',
-                    frequency: .02782
-                },
-                {
-                    letter: 'D',
-                    frequency: .04253
-                },
-                {
-                    letter: 'E',
-                    frequency: .12702
-                },
-                {
-                    letter: 'F',
-                    frequency: .02288
-                },
-                {
-                    letter: 'G',
-                    frequency: .02015
-                },
-                {
-                    letter: 'H',
-                    frequency: .06094
-                },
-                {
-                    letter: 'I',
-                    frequency: .06966
-                },
-                {
-                    letter: 'J',
-                    frequency: .00153
-                },
-                {
-                    letter: 'K',
-                    frequency: .00772
-                },
-                {
-                    letter: 'L',
-                    frequency: .04025
-                },
-                {
-                    letter: 'M',
-                    frequency: .02406
-                },
-                {
-                    letter: 'N',
-                    frequency: .06749
-                },
-                {
-                    letter: 'O',
-                    frequency: .07507
-                },
-                {
-                    letter: 'P',
-                    frequency: .01929
-                },
-                {
-                    letter: 'Q',
-                    frequency: .00095
-                },
-                {
-                    letter: 'R',
-                    frequency: .05987
-                },
-                {
-                    letter: 'S',
-                    frequency: .06327
-                },
-                {
-                    letter: 'T',
-                    frequency: .09056
-                },
-                {
-                    letter: 'U',
-                    frequency: .02758
-                },
-                {
-                    letter: 'V',
-                    frequency: .00978
-                },
-                {
-                    letter: 'W',
-                    frequency: .02360
-                },
-                {
-                    letter: 'X',
-                    frequency: .00150
-                },
-                {
-                    letter: 'Y',
-                    frequency: .01974
-                },
-                {
-                    letter: 'Z',
-                    frequency: .00074
-                }
-            ];
+            dataset = aTestDataSet()
+                .withLettersFrequency()
+                .build();
             barChart = chart();
 
             // DOM Fixture Setup
@@ -138,6 +39,11 @@ define(['jquery', 'd3', 'bar'], function($, d3, chart) {
             expect(containerFixture.select('g.chart-group').empty()).toBeFalsy();
             expect(containerFixture.select('g.x-axis-group').empty()).toBeFalsy();
             expect(containerFixture.select('g.y-axis-group').empty()).toBeFalsy();
+            expect(containerFixture.select('g.grid-lines-group').empty()).toBeFalsy();
+        });
+
+        it('should render grid lines', () => {
+            expect(containerFixture.select('.horizontal-grid-line').empty()).toBeFalsy();
         });
 
         it('should render an X and Y axis', () => {
