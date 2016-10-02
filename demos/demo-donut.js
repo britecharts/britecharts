e3d3dcb60f29d633cd32930a8f9bf93c66ec7ee9
Marcos Iglesias <miglesias@eventbrite.com>
2016-10-01 17:30:00 -0700

    Updating donut tests

--- demos/demo-donut.js
+++ demos/demo-donut.js
@@ -5,43 +5,13 @@ var _ = require('underscore'),
 
     donut = require('./../src/charts/donut'),
     legend = require('./../src/charts/legend'),
+    dataBuilder = require('./../test/fixtures/donutChartDataBuilder'),
 
-    dataset = [
-              {
-                'name': 'Valentines VIP special',
-                'id': 33571136,
-                'quantity': 4000,
-                'quantity_human': '4000',
-                'percentage': 4
-              },
-              {
-                'name': 'Groupon 2 - Pack',
-                'id': 32913850,
-                'quantity': 15000,
-                'quantity_human': '15000',
-                'percentage': 15
-              },
-              {
-                'name': 'Groupon Individual Runner',
-                'id': 32913849,
-                'quantity': 10000,
-                'quantity_human': '10000',
-                'percentage': 10
-              },
-              {
-                'name': 'LivingSocial Individual Runner',
-                'id': 32780575,
-                'quantity': 6000,
-                'quantity_human': '6000',
-                'percentage': 6
-              },
-              {
-                'name': 'Other',
-                'id': 0,
-                'quantity': 65000,
-                'percentage': 65
-              }
-        ],
+    briteChartsColors = ['#f6682f', '#f20cb6', '#ff0044', '#ffdb00', '#00cc52', '#00a8f2'],
+
+    dataset = new dataBuilder.DonutDataBuilder()
+        .withFivePlusOther()
+        .build(),
     legendChart;
 
 function createDonutChart(dataset, legendChart) {
@@ -58,6 +28,7 @@ function createDonutChart(dataset, legendChart) {
         .height(containerWidth)
         .externalRadius(containerWidth/2.5)
         .internalRadius(containerWidth/5)
+        .colorScheme(briteChartsColors)
         .on('customMouseOver', function(data) {
             legendChart.highlight(data.data.id);
         })
@@ -78,49 +49,6 @@ function getLegendChart(dataset) {
 
 function createSmallDonutChart() {
     var donutChart = donut(),
-        dataset = [
-              {
-                'name': 'VALENTINES VIP SPECIAL',
-                'id': 33571136,
-                'quantity': 86,
-                'quantity_human': '86',
-                'percentage': 3
-              },
-              {
-                'name': 'Groupon 4 - Pack',
-                'id': 32913851,
-                'quantity': 300,
-                'quantity_human': '300',
-                'percentage': 10
-              },
-              {
-                'name': 'Groupon 2 - Pack',
-                'id': 32913850,
-                'quantity': 276,
-                'quantity_human': '276',
-                'percentage': 10
-              },
-              {
-                'name': 'Groupon Individual Runner',
-                'id': 32913849,
-                'quantity': 195,
-                'quantity_human': '195',
-                'percentage': 10
-              },
-              {
-                'name': 'LivingSocial Individual Runner',
-                'id': 32780575,
-                'quantity': 36,
-                'quantity_human': '36',
-                'percentage': 1
-              },
-              {
-                'name': 'Other',
-                'id': 0,
-                'quantity': 5814,
-                'percentage': 65
-              }
-        ],
         donutContainer = d3.select('.js-small-donut-chart-container'),
         containerWidth = donutContainer.node().getBoundingClientRect().width;
 
