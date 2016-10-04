a1b47392fca51cb71a82985cbc1a2f3eccef5465
Marcos Iglesias <miglesias@eventbrite.com>
2016-10-03 21:24:42 -0700

    Tests

--- test/specs/tooltip.spec.js
+++ test/specs/tooltip.spec.js
@@ -46,85 +46,6 @@ define(['jquery', 'd3', 'tooltip'], function($, d3, tooltip) {
             expect(containerFixture.select('.britechart-tooltip').style('display')).toBe('block');
         });
 
-        it('should update the title of the tooltip', () =>  {
-            tooltipChart.update({
-                date: '2015-08-05T07:00:00.000Z',
-                topics: []
-            }, topicColorMap, 0);
-
-            expect(
-                containerFixture.select('.britechart-tooltip')
-                    .selectAll('.tooltip-title')
-                    .text()
-            ).toBe('Tooltip title - Aug 05, 2015');
-        });
-
-        it('should add a line of text for each topic', () =>  {
-            tooltipChart.update({
-                date: '2015-08-05T07:00:00.000Z',
-                topics: [
-                    {
-                        name: 103,
-                        value: '5',
-                        topicName: 'San Francisco'
-                    },
-                    {
-                        name: 60,
-                        value: '10',
-                        topicName: 'Chicago'
-                    }
-                ]
-            }, topicColorMap, 0);
-
-            expect(
-                containerFixture.select('.britechart-tooltip')
-                    .selectAll('.tooltip-left-text')
-                    .size()
-            ).toEqual(2);
-        });
-
-        it('should add a circle for each topic', () =>  {
-            tooltipChart.update({
-                date: '2015-08-05T07:00:00.000Z',
-                topics: [
-                    {
-                        name: 103,
-                        value: 0,
-                        topicName: 'San Francisco'
-                    },
-                    {
-                        name: 60,
-                        value: 10,
-                        topicName: 'Chicago'
-                    }
-                ]
-            }, topicColorMap, 0);
-
-            expect(
-                containerFixture.select('.britechart-tooltip')
-                    .selectAll('.tooltip-circle')
-                    .size()
-            ).toEqual(2);
-        });
-
-        // TODO: develop
-        it('should position the tooltip in the right coordinates', () =>  {
-            tooltipChart.update({
-                date: '2015-08-05T07:00:00.000Z',
-                topics: [
-                    {
-                        name: 103,
-                        value: 0,
-                        topicName: 'San Francisco'
-                    }
-                ]
-            }, topicColorMap, 10);
-
-            expect(
-                containerFixture.select('.tooltip-text').attr('transform')
-            ).toEqual('translate(65,-55)');
-        });
-
         xit('should resize the tooltip depending of number of topics', () =>  {
             tooltipChart.update({
                 date: '2015-08-05T07:00:00.000Z',
@@ -164,16 +85,83 @@ define(['jquery', 'd3', 'tooltip'], function($, d3, tooltip) {
             ).toEqual('105');
         });
 
-        it('should provide title getter and setter', () => {
-            let defaultTitle = tooltipChart.title(),
-                testTitle = 'test',
-                newTitle;
+        describe('Render', function() {
 
-            tooltipChart.title(testTitle);
-            newTitle = tooltipChart.title();
+            it('should update the title of the tooltip', () =>  {
+                tooltipChart.update({
+                    date: '2015-08-05T07:00:00.000Z',
+                    topics: []
+                }, topicColorMap, 0);
 
-            expect(defaultTitle).not.toBe(testTitle);
-            expect(newTitle).toBe(testTitle);
+                expect(
+                    containerFixture.select('.britechart-tooltip')
+                        .selectAll('.tooltip-title')
+                        .text()
+                ).toBe('Tooltip title - Aug 05, 2015');
+            });
+
+            it('should add a line of text for each topic', () =>  {
+                tooltipChart.update({
+                    date: '2015-08-05T07:00:00.000Z',
+                    topics: [
+                        {
+                            name: 103,
+                            value: '5',
+                            topicName: 'San Francisco'
+                        },
+                        {
+                            name: 60,
+                            value: '10',
+                            topicName: 'Chicago'
+                        }
+                    ]
+                }, topicColorMap, 0);
+
+                expect(
+                    containerFixture.select('.britechart-tooltip')
+                        .selectAll('.tooltip-left-text')
+                        .size()
+                ).toEqual(2);
+            });
+
+            it('should add a circle for each topic', () =>  {
+                tooltipChart.update({
+                    date: '2015-08-05T07:00:00.000Z',
+                    topics: [
+                        {
+                            name: 103,
+                            value: 0,
+                            topicName: 'San Francisco'
+                        },
+                        {
+                            name: 60,
+                            value: 10,
+                            topicName: 'Chicago'
+                        }
+                    ]
+                }, topicColorMap, 0);
+
+                expect(
+                    containerFixture.select('.britechart-tooltip')
+                        .selectAll('.tooltip-circle')
+                        .size()
+                ).toEqual(2);
+            });
+        });
+
+        describe('API', function() {
+
+            it('should provide title getter and setter', () => {
+                let current = tooltipChart.title(),
+                    expected = 'test',
+                    actual;
+
+                tooltipChart.title(expected);
+                actual = tooltipChart.title();
+
+                expect(current).not.toBe(expected);
+                expect(actual).toBe(expected);
+            });
         });
     });
 });
