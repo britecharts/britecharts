567545d48ba9c50843fff6b8c8206ef59063f72b
Ryan Wholey <rwholey@eventbrite.com>
2017-02-07 17:26:41 -0800

    pick x axis smarter

--- src/charts/helpers/constants.js
+++ src/charts/helpers/constants.js
@@ -1,5 +1,20 @@
 define(function(require) {
+    const axisTimeCombinations = {
+        MINUTE_HOUR: 'minute-hour',
+        HOUR_DAY: 'hour-day',
+        DAY_MONTH: 'day-month',
+        MONTH_YEAR: 'month-year'
+    };
+
+    const timeBenchmarks= {
+        ONE_AND_A_HALF_YEARS: 47304000000,
+        ONE_DAY: 86400001
+    };
+
     return {
+        axisTimeCombinations,
+        timeBenchmarks,
         lineGradientId: 'lineGradientId'
     };
 });
+
