932ea6488f595cd0de6d53406d9e1e9c260dbfbf
Marcos Iglesias <golodhros@gmail.com>
2017-02-09 11:30:27 -0800

    Retrieve old demo look

--- demos/demo-line.js
+++ demos/demo-line.js
@@ -24,7 +24,7 @@ function createLineChart(optionalColorSchema) {
             lineChart1.exportChart('linechart.png', 'Britecharts Line Chart');
         });
 
-        dataset = testDataSet.withHourDateRange().build();
+        dataset = testDataSet.with5Topics().build();
 
         // LineChart Setup and start
         lineChart1
@@ -49,7 +49,8 @@ function createLineChart(optionalColorSchema) {
 
         // Tooltip Setup and start
         chartTooltip
-            .forceDateRange(chartTooltip.axisTimeCombinations.HOUR_DAY)
+            // In order to change the date range on the tooltip title, uncomment this line
+            // .forceDateRange(chartTooltip.axisTimeCombinations.HOUR_DAY)
             .title('Quantity Sold');
 
         // Note that if the viewport width is less than the tooltipThreshold value,
