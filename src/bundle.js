2310489d165cc46c91abfda8e5bdd89a5684b705
Marcos Iglesias <golodhros@gmail.com>
2017-05-11 22:50:43 -0700

    Fixing bundle setup

---
+++ src/bundle.js
@@ -0,0 +1,25 @@
+import bar from './charts/bar.js';
+import donut from './charts/donut.js';
+import legend from './charts/legend.js';
+import line from './charts/line.js';
+import tooltip from './charts/tooltip.js';
+import miniTooltip from './charts/mini-tooltip.js';
+import sparkline from './charts/sparkline.js';
+import stackedArea from './charts/stacked-area.js';
+import step from './charts/step.js';
+import brush from './charts/brush.js';
+import colors from './charts/helpers/colors.js';
+
+export {
+        bar,
+        donut,
+        legend,
+        line,
+        tooltip,
+        miniTooltip,
+        sparkline,
+        stackedArea,
+        step,
+        brush,
+        colors
+    };
