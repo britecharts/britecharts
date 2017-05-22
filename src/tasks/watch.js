099b7125a81daf5f97a60fc365455dc9d6ce1ccf
Bung <zh.bung@gmail.com>
2017-05-22 23:22:05 +0800

    Fix docs merged (#253)
    * fix readme demos links while view docs on local&& use webpack-dev-server --open option to open browser
    
    * use grunt-sync to sync static files && make demos page as partial of layout && watch docs related files
    
    * remove moveDemos task
    
    * fix sync task
    
    * change bundle.js scrpit src to /assets/bundle.js so webpack dev server knows which page need livereload
    
    * fix grunt watch task
    
    * config  publicPath to /assets/ and then change layout bundle.js script src to /assets/bundle.js so webpack dev server knows which page need livereload
    
    * add pathRewrite rule if you open browser at http://localhost:8001/britecharts the url path will be same as github.io

--- src/tasks/watch.js
+++ src/tasks/watch.js
@@ -2,9 +2,38 @@ module.exports = function(grunt) {
     'use strict';
 
     grunt.config.set('watch', {
-        scripts: {
-            files: ['demos/**/*'],
-            tasks: ['moveDemos']
+        readme: {
+            files: ['README.md'],
+            tasks: ['jsdoc:dist']
         },
+        // scripts: {  //since we use webpack-dev-server hot reload no need watch bundle.js
+        //     files:['demos/build/**'],
+        //     tasks:['sync:scripts']
+        // },
+        styles:{
+            files:['demos/css/**'],
+            tasks:['sync:fonts']
+        },
+        fonts:{
+            files:['src/doc/template/static/fonts/**'],
+            tasks:['sync:fonts']
+            
+        },
+        manifest:{
+            files:['src/doc/template/manifest.json'],
+            tasks:['sync:manifest']
+        },
+        demos:{
+            files:['demos/*.html'],
+            tasks: ['jsdoc:dist']
+        },
+        tmpl:{
+            files:['src/doc/template/tmpl/*.tmpl'],
+            tasks: ['jsdoc:dist']
+        },
+        json:{
+            files:['demos/*.json'],
+            tasks: ['jsdoc:dist']
+        }
     });
 }
