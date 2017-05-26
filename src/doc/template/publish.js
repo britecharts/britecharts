952abad42f2293ac7c2187baaecd47bc95c731c2
Bung <zh.bung@gmail.com>
2017-05-25 21:55:22 -0700

    Bung87 docs header (#273)
    * mainpage.tmpl fix replace regex
    
    * fix readme doc
    
    * include demo js common part && add Getting Started nav link
    
    * debug some info
    
    * seperate demo bundle.js to certains page
    
    * fix demo-kitchen
    
    * remove .page-title from contaniner.tmpl while doc.kind === 'mainpage'
    
    * move cdn page out of demos/
    
    * rm docs/tutorial-cdn.html
    
    * fix nav
    
    * use demo-scripts-seperate branch generated docs
    
    * index page title use conf.meta.title
    
    * add generated docs
    
    * add demo-kitchen-sink.js avoid 404 since we include demo scrpit default even doest exist one
    
    * Adding CDN references, patching search form location on different viewports

--- src/doc/template/publish.js
+++ src/doc/template/publish.js
@@ -30,7 +30,8 @@ var template = require('jsdoc/template'),
 
 var globalUrl = helper.getUniqueFilename('global');
 var indexUrl = helper.getUniqueFilename('index');
-
+var gettingUrl = helper.getUniqueFilename('getting-started');
+var cdnUrl = helper.getUniqueFilename("cdn");
 var navOptions = {
   includeDate: conf.includeDate !== false,
   logoFile: conf.logoFile,
@@ -57,8 +58,18 @@ var searchableDocuments = {};
 
 var navigationMaster = {
   index: {
-    title: navOptions.systemName,
+    title: conf.meta.title,
     link: indexUrl,
+    members: []
+  },
+  gettingStarted:{
+    title:"Getting started",
+    link:gettingUrl,
+    members: []
+  },
+  cdn:{
+    title:"CDN",
+    link:cdnUrl,
     members: []
   },
   namespace: {
@@ -66,8 +77,13 @@ var navigationMaster = {
     link: helper.getUniqueFilename("namespaces.list"),
     members: []
   },
+  tutorial: {
+    title: "Demos",
+    link: helper.getUniqueFilename("tutorials.list"),
+    members: []
+  },
   module: {
-    title: "API",
+    title: "Modules",
     link: helper.getUniqueFilename("modules.list"),
     members: []
   },
@@ -89,11 +105,6 @@ var navigationMaster = {
   interface: {
     title: "Interfaces",
     link: helper.getUniqueFilename("interfaces.list"),
-    members: []
-  },
-  tutorial: {
-    title: "Demos",
-    link: helper.getUniqueFilename("tutorials.list"),
     members: []
   },
   global: {
@@ -467,16 +478,23 @@ function buildNav(members) {
     }
   }
 
-  var topLevelNav = [];
+  var topLevelNav = [],apis =[];
   _.each(nav, function(entry, name) {
-    if (entry.members.length > 0 && name !== "index") {
-      topLevelNav.push({
-        title: entry.title,
-        link: entry.link,
-        members: entry.members
+    if (entry.members.length > 0 && name !== "index" && name!="tutorial") {
+      entry.members.forEach(function(c){
+        apis.push( c )
       });
     }
   });
+  topLevelNav.push(nav.gettingStarted);
+  topLevelNav.push(nav.tutorial);
+    topLevelNav.push(nav.cdn);
+  // topLevelNav.push(nav.gettingStarted);
+    topLevelNav.push({
+        title: "API",
+        link: linkto("modules.list.html"),
+        members: apis
+      });
   nav.topLevelNav = topLevelNav;
 }
 
@@ -500,6 +518,8 @@ exports.publish = function(taffyData, opts, tutorials) {
 
   //	var globalUrl = helper.getUniqueFilename( 'global' );
   helper.registerLink('global', globalUrl);
+
+ 
 
   // set up templating
   // set up templating
@@ -772,7 +792,7 @@ exports.publish = function(taffyData, opts, tutorials) {
       kind: 'package'
     });
 
-  generate('index', 'Index',
+  generate('index', navigationMaster.index.title,
     packages.concat(
       [{
         kind: 'mainpage',
@@ -781,6 +801,29 @@ exports.publish = function(taffyData, opts, tutorials) {
       }]
     ).concat(files),
     indexUrl);
+var markdown = require('jsdoc/util/markdown');
+var parser = markdown.getParser();
+
+
+var content = fs.readFileSync((`${process.cwd()}/` + './GETTINGSTARTED.md')).toString();
+var html = parser(content);
+    generate('getting-started', 'Getting Started with Britecharts',
+    [{
+      kind:'mainpage',
+      readme:html
+    }],
+    gettingUrl);
+
+
+
+    generate('cdn', 'CDN Loading Demo - Britecharts',
+    [{
+      kind:'mainpage',
+      readme:'<div class="embed-responsive embed-responsive-16by9" style="height:1833px;" ><iframe height="1833" scrolling="no" style="height:1833px;" class="embed-responsive-item" frameborder="0" src="cdn-iframe.html" allowfullscreen></iframe></div>'
+    }],
+    cdnUrl);
+
+    
 
   // set up the lists that we'll use to generate pages
   var classes = taffy(members.classes);
@@ -840,6 +883,8 @@ exports.publish = function(taffyData, opts, tutorials) {
   function generateTutorial(title, tutorial, filename) {
     var tutorialData = {
       title: title,
+      name: tutorial.name,
+      filename:filename,
       header: tutorial.title,
       content: tutorial.parse(),
       children: tutorial.children,
