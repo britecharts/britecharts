de9190f32876be852eb9ecbe1ef8088671167b7d
Ryan Wholey <rjwholey@gmail.com>
2017-08-09 16:21:55 -0700

    use yarn for dependency management cli (#329)
    * use yarn for dependency management cli
    
    * remove reggie due to unwanted sub dep [dtrace-provider]

--- src/tasks/release.js
+++ src/tasks/release.js
@@ -23,21 +23,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('publish', 'Publish package to reggie', function() {
-
-        var pkg = grunt.file.readJSON('package.json'),
-            cmd = './node_modules/reggie/client.js -u ' + pkg.publishConfig.registry + ' publish';
-
-        if (grunt.option('no-write')) {
-            grunt.log.ok('---DRY-RUN----');
-            grunt.log.ok(cmd);
-        } else {
-            grunt.log.ok(cmd);
-            shell.exec(cmd);
-        }
-        grunt.log.ok('Published to Reggie');
-    });
-
     grunt.registerTask('release', function() {
 
         var bumpTask = 'bump-version' + Array.prototype.slice.call(arguments).map(function(val) {
@@ -45,7 +30,6 @@ module.exports = function(grunt) {
         });
 
         grunt.task.run(bumpTask);
-        // grunt.task.run(bumpTask, 'publish', 'bump-dist');
     });
 
     grunt.registerTask('bump-dist', function() {
