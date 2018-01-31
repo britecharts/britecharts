fc34af7f6e9121adf738f11993eb1c318ac2f8f8
Matthieu H <matthieu@sociallymap.com>
2018-01-31 18:25:36 +0100

    Test: reverseStacks option for stacked-bar

--- test/specs/stacked-bar.spec.js
+++ test/specs/stacked-bar.spec.js
@@ -210,6 +210,18 @@ define(['d3', 'stacked-bar', 'stackedBarDataBuilder'], function(d3, chart, dataB
                 expect(actual).toBe(expected);
             });
 
+            it('should provide reverseStacks getter and setter', () => {
+                let previous = stackedBarChart.reverseStacks(),
+                    expected = true,
+                    actual;
+
+                stackedBarChart.reverseStacks(expected);
+                actual = stackedBarChart.reverseStacks();
+
+                expect(previous).not.toBe(actual);
+                expect(actual).toBe(expected);
+            });
+
             it('should provide isAnimated getter and setter', () => {
                 let previous = stackedBarChart.isAnimated(),
                     expected = true,
