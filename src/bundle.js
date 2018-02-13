7db8d6aeddf6eefc68f5fc4a4acbfb0f4f4e15b2
Marcos Iglesias <golodhros@gmail.com>
2018-02-13 15:06:06 -0800

    Updating bundle paths (#512)

--- src/bundle.js
+++ src/bundle.js
@@ -2,7 +2,7 @@ import bar from './charts/bar.js';
 import donut from './charts/donut.js';
 import legend from './charts/legend.js';
 import line from './charts/line.js';
-import loadingStates from './charts/helpers/loadingStates.js';
+import loadingStates from './charts/helpers/load.js';
 import tooltip from './charts/tooltip.js';
 import miniTooltip from './charts/mini-tooltip.js';
 import sparkline from './charts/sparkline.js';
@@ -11,7 +11,7 @@ import groupedBar from './charts/grouped-bar.js';
 import stackedBar from './charts/stacked-bar.js';
 import step from './charts/step.js';
 import brush from './charts/brush.js';
-import colors from './charts/helpers/colors.js';
+import colors from './charts/helpers/color.js';
 
 export {
         bar,
