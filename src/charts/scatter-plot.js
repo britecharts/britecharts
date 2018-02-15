e1c37e647217319da5349141fd872046b272dc4e
Daler Asrorov <daler.asrorov@eventbrite.com>
2018-02-15 13:20:22 -0800

    Added more libraries

--- src/charts/scatter-plot.js
+++ src/charts/scatter-plot.js
@@ -4,6 +4,18 @@ define(function(require) {
     const d3Array = require('d3-array');
     const d3Axis = require('d3-axis');
     const d3Collection = require('d3-collection');
+    const d3Dispatch = require('d3-dispatch');
+    const d3Ease = require('d3-ease');
+    const d3Format = require('d3-format');
+    const d3Scale = require('d3-scale');
+    const d3Shape = require('d3-shape');
+    const d3Selection = require('d3-selection');
+    const d3Transition = require('d3-transition');
+    const d3TimeFormat = require('d3-time-format');
+
+    const {exportChart} = require('./helpers/export');
+    const colorHelper = require('./helpers/color');
+    const {scatterPlot} = require('./helpers/load');
 
 
 });
