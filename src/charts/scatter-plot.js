1be2264df0d682b9eb0cc60fb5c5d5211b32ba4a
Daler Asrorov <daler.asrorov@eventbrite.com>
2018-03-17 23:54:03 -0700

    Added comments to Chart API documentation

--- src/charts/scatter-plot.js
+++ src/charts/scatter-plot.js
@@ -15,7 +15,92 @@ define(function(require) {
 
     const {exportChart} = require('./helpers/export');
     const colorHelper = require('./helpers/color');
-    const {scatterPlot} = require('./helpers/load');
 
+    const {
+        formatIntegerValue,
+        formatDecimalValue,
+        isInteger,
+        uniqueId
+    } = require('./helpers/number');
 
+    /**
+     * @typedef D3Selection
+     * @type {Array[]}
+     * @property {Number} length            Size of the selection
+     * @property {DOMElement} parentNode    Parent of the selection
+     */
+
+     /**
+      * @typedef scatterPlotDataByTopic
+      * @type {Object}
+      * @property {String} topic    The name of the topic or category
+      * @property {Number} x        Position of the data point with respect to x-axis
+      * @property {Number} y        Position of the data point with respect to y-axis
+      * @property {Number} [size]   Relative size of the data point (optional)
+      *
+      * @example
+      * {
+      *     topic: 'topic',
+      *     x:  123,
+      *     y: 234,
+      *     size: 345
+      * }
+      */
+
+     /**
+      * @typedef ScatterPlotData
+      * @type {Array[]}
+      * @property {scatterPlotDataByTopic[]} dataByTopic   Data values to chart (required)
+      *
+      * @example
+      * [
+      *     {
+      *         topic: 'topic',
+      *         x:  123,
+      *         y: 234,
+      *         size: 345
+      *      },
+      *      {
+      *         topic: 'topic1',
+      *         x: 23,
+      *         y: 4,
+      *         size: 45
+      *      },
+      *      {
+      *         topic: 'topic',
+      *         x: 12,
+      *         y: 31,
+      *         size: 22
+      *      }
+      *  ]
+      */
+
+     /**
+      * Scatter Plot reusable API module that allows us to
+      * render a chart with data points and configurable chart.
+      * @module ScatterPlot
+      * @tutorial scatterPlot
+      * @requires d3-array, d3-axis, d3-ease, d3-format, d3-scale, d3-shape, d3-selection
+      *
+      * @example
+      * let scatterPlot = scatterPlot();
+      * 
+      * scatterPlot
+      *     .width(500)
+      *     .height(500);
+      * 
+      * d3Selection.select('.css-selector)
+      *     .datum(dataset)
+      *     .call(scatterPlot);
+      */
+    return function module() {
+        
+        function exports(_selection) {
+            _selection.each(function(_data) {
+                console.log('this data', _data);
+            });    
+        }
+
+        return exports;
+    };
 });
