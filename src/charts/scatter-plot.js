637b1ea736fd8cb8faedd382870b470fa5149ef4
Daler Asrorov <daler.asrorov@eventbrite.com>
2018-03-18 00:01:21 -0700

    revised the docs/demo scatter plot title

--- src/charts/scatter-plot.js
+++ src/charts/scatter-plot.js
@@ -78,8 +78,8 @@ define(function(require) {
      /**
       * Scatter Plot reusable API module that allows us to
       * render a chart with data points and configurable chart.
-      * @module ScatterPlot
-      * @tutorial scatterPlot
+      * @module Scatter-plot
+      * @tutorial scatter-plot
       * @requires d3-array, d3-axis, d3-ease, d3-format, d3-scale, d3-shape, d3-selection
       *
       * @example
