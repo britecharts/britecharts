28fbe5280b59fa3ad30b6b94136e8ad0381a0177
Daler Asrorov <31934144+dalerasrorov-eb@users.noreply.github.com>
2018-03-21 14:26:42 -0700

    Implemented initial draft of Scatter Plot (#560)
    * Initial files added
    
    * Added some modules
    
    * Added more libraries
    
    * Add demo for scatter plot
    
    * Scatter plot added to webpack configs and readme
    
    * Added comments to Chart API documentation
    
    * revised the docs/demo scatter plot title
    
    * imrpove docs
    
    * Apply scatter plot attributes
    
    * Revised scatter data API properties and fixed demo connection
    
    * Created scatter plot data builder with json
    
    * First datum call
    
    * Make data array format from data builder
    
    * Created basic clean data function
    
    * Add container append and svg
    
    * Update example name demo
    
    * Added x and y scales
    
    * build scales properly
    
    * Added first scatter plot tests
    
    * Added chart margin tests
    
    * remove numeric reference
    
    * Built color scales
    
    * Return values in comments changed
    
    * Added colorSchema setter/getter with API test
    
    * Draw x and y axis
    
    * Draws data points
    
    * Add point color
    
    * Created area scale
    
    * New parameter - max circle area for the plot
    
    * Ability to switch between animated or not
    
    * remove todo comments
    
    * alignment
    
    * cursor pointer over points
    
    * Public xTicks and yTicks getters/setters
    
    * revise api comments
    
    * Added more foundation and render tests
    
    * code style fixes
    
    * removed trailing spaces
    
    * Added scatter plto CSS
    
    * add scatter plot sass file
    
    * show x axis and hide y instead
    
    * keep ascending order
    
    * idiotic spelling error that cost 5 hours
    
    * Apply axis none classes to scatter plot
    
    * Applied correct styles to css
    
    * Export chart added with test
    
    * draw vertical grid lines
    
    * draw gridlines implemented
    
    * Added new tests for gridlines
    
    * revert bar change
    
    * Added dispatcher and first custom handler - click
    
    * better test grid lines desc
    
    * Fixed click test
    
    * Remove unnecessary code and NIT
    
    * remove more comments
    
    * Fixed comment alignment
    
    * Make grid optional
    
    * Made grid lines optional, added to the test
    
    * Updated min req test

---
+++ test/specs/scatter-plot.spec.js
@@ -0,0 +1,213 @@
+define(['d3', 'scatter-plot', 'scatterPlotDataBuilder'], function(d3, chart, dataBuilder) {
+    'use strict';
+
+    const aTestDataSet = () => new dataBuilder.ScatterPlotDataBuilder();
+    const buildDataSet = (dataSetName) => {
+        return aTestDataSet()
+            [dataSetName]()
+            .build();
+    };
+
+    describe('Scatter Plot', () => {
+
+        let scatterPlot, dataset, containerFixture, f;
+
+        beforeEach(() => {
+            dataset = buildDataSet('withFourNames');
+            scatterPlot = chart()
+                    .grid('full');
+
+            // DOM Fixture Setup
+            f = jasmine.getFixtures();
+            f.fixturesPath = 'base/test/fixtures/';
+            f.load('testContainer.html');
+
+            containerFixture = d3.select('.test-container');
+            containerFixture.datum(dataset).call(scatterPlot);
+        });
+
+        afterEach(() => {
+            containerFixture.remove();
+            f = jasmine.getFixtures();
+            f.cleanUp();
+            f.clearCache();
+        });
+
+        it('should render a chart with minimal requirements', () => {
+            let expected = 1;
+            let actual = containerFixture.select('.scatter-plot').nodes().length;
+
+            expect(actual).toEqual(expected);
+        });
+
+        it('should render container, axis and chart groups', () => {
+            expect(containerFixture.select('g.container-group').empty()).toBeFalsy();
+            expect(containerFixture.select('g.chart-group').empty()).toBeFalsy();
+            expect(containerFixture.select('g.x-axis-group').empty()).toBeFalsy();
+            expect(containerFixture.select('g.y-axis-group').empty()).toBeFalsy();
+            expect(containerFixture.select('g.grid-lines-group').empty()).toBeFalsy();
+            expect(containerFixture.select('g.metadata-group').empty()).toBeFalsy();
+        });
+
+        it('should render an X and Y axis', () => {
+            expect(containerFixture.select('.x-axis-group .axis.x').empty()).toBeFalsy();
+            expect(containerFixture.select('.y-axis-group .axis.y').empty()).toBeFalsy();
+        });
+
+        it('should render horizontal grid lines', () => {
+            expect(containerFixture.select('.horizontal-grid-line').empty()).toBeFalsy();
+        });
+
+        it('should render vertical grid lines', () => {
+            expect(containerFixture.select('.vertical-grid-line').empty()).toBeFalsy();
+        });
+
+        describe('API', function() {
+
+            it('should provide colorSchema getter and setter', () => {
+                let previous = scatterPlot.colorSchema(),
+                    expected = ['#aaa', '#bbb', '#ccc', '#fff'],
+                    actual;
+
+                scatterPlot.colorSchema(expected);
+                actual = scatterPlot.colorSchema();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toEqual(expected);
+            });
+
+            it('should have exportChart defined', () => {
+                expect(scatterPlot.exportChart).toBeDefined();
+            });
+
+            it('should provide grid mode getter and setter', () => {
+                let previous = scatterPlot.grid(),
+                    expected = 'vertical',
+                    actual;
+
+                scatterPlot.grid(expected);
+                actual = scatterPlot.grid();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toBe(expected);
+            });
+
+            it('should provide height getter and setter', () => {
+                let previous = scatterPlot.width(),
+                    expected = 200,
+                    actual;
+
+                scatterPlot.height(expected);
+                actual = scatterPlot.height();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toBe(expected);
+            });
+
+            it('should provide isAnimated getter and setter', () => {
+                let previous = scatterPlot.isAnimated(),
+                    expected = false,
+                    actual;
+
+                scatterPlot.isAnimated(expected);
+                actual = scatterPlot.isAnimated();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toEqual(expected);
+            });
+
+            it('should provide margin getter and setter', () => {
+                let previous = scatterPlot.margin(),
+                    expected = {top: 4, right: 4, bottom: 4, left: 4},
+                    actual;
+
+                scatterPlot.margin(expected);
+                actual = scatterPlot.margin();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toEqual(expected);
+            });
+
+            it('should provide maxCircleArea getter and setter', () => {
+                let previous = scatterPlot.maxCircleArea(),
+                    expected = 25,
+                    actual;
+
+                scatterPlot.maxCircleArea(expected);
+                actual = scatterPlot.maxCircleArea();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toEqual(expected);
+            });
+
+            it('should provide xTicks getter and setter', () => {
+                let previous = scatterPlot.xTicks(),
+                    expected = 48,
+                    actual;
+
+                scatterPlot.xTicks(expected);
+                actual = scatterPlot.xTicks();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toBe(expected);
+            });
+
+            it('should provide yTicks getter and setter', () => {
+                let previous = scatterPlot.yTicks(),
+                    expected = 48,
+                    actual;
+
+                scatterPlot.yTicks(expected);
+                actual = scatterPlot.yTicks();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toBe(expected);
+            });
+
+            it('should provide width getter and setter', () => {
+                let previous = scatterPlot.width(),
+                    expected = 200,
+                    actual;
+
+                scatterPlot.width(expected);
+                actual = scatterPlot.width();
+
+                expect(previous).not.toBe(expected);
+                expect(actual).toBe(expected);
+            });
+        });
+
+        describe('when margins are set partially', function() {
+
+            it('should override the default values', () => {
+                let previous = scatterPlot.margin(),
+                expected = {
+                    ...previous,
+                    top: 10,
+                    right: 20
+                },
+                actual;
+
+                scatterPlot.margin(expected);
+                actual = scatterPlot.margin();
+
+                expect(previous).not.toBe(actual);
+                expect(actual).toEqual(expected);
+            })
+        });
+
+        describe('when clicking on a point', function () {
+
+            it('should trigger a callback on mouse click', () => {
+                let callbackSpy = jasmine.createSpy('callback');
+                let scatterDataPoint = containerFixture.select('.chart-group circle');
+
+                scatterPlot.on('customClick', callbackSpy);
+                scatterDataPoint.dispatch('click');
+
+                expect(callbackSpy.calls.count()).toBe(1);
+                expect(callbackSpy.calls.allArgs()[0].length).toBe(3);
+            });
+        });
+    });
+});
