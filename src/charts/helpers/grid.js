a5c9f8118262d84fc724c659edb16e1353395d63
Brendan Ratter <email@brendanratter.net>
2018-03-24 18:32:22 -0500

    Refactored grid helper and added jsdocs

--- src/charts/helpers/grid.js
+++ src/charts/helpers/grid.js
@@ -1,3 +1,7 @@
+// TODO: Document d3 objects rather than using *
+// TODO: Add bi-directional accessors for 2d grid, and determine naming (H/V vs. X/Y)
+// TODO: Refactor positioning animation functions into helpers
+
 define(function(require) {
     'use strict';
 
@@ -13,21 +17,35 @@ define(function(require) {
         V: 'vertical'
     }
 
-    // Default positioning function for continuous scales
-    // The +0.5 avoids anti-aliasing artifacts
-    function number(scale) {
+    /**
+     * Higher order function that returns the default positioning function for continuous scales
+     * The +0.5 avoids anti-aliasing artifacts
+     * @param {*} scale - Scale for positioning
+     * @return {function}
+     */
+    function positionNumber(scale) {
         return d => +scale(d) + 0.5;
     }
 
-    // Replacement positioning function for banded scales
-    // Also adjusted for anti-aliasing
-    function center(scale) {
+    /**
+     * Higher order function that returns the positioning function for bandwidth scales
+     * Also adjusted for anti-aliasing
+     * @param {*} scale - Scale for positioning
+     * @return {function}
+     */
+    function positionCenter(scale) {
         let offset = Math.max(0, scale.bandwidth() - 1) / 2;
 
         if (scale.round()) offset = Math.round(offset);
         return d => +scale(d) + offset + 0.5;
     }
 
+    /**
+     * Constructor for a one-dimensional grid helper
+     * @param {string} orient - orientation string to define the direction
+     * @param {*} scaleY - d3 scale for the grid's ticks
+     * @return {gridBaseGenerator}
+     */
     function gridBase(orient, scale) {
         let range = [0, 1],
             offsetStart = 0,
@@ -36,25 +54,45 @@ define(function(require) {
             ticks = null,
             tickValues = null,
             
+            // Create a class array helper for producing class lists
             classArr = classArray('grid', orient),
+
+            // Manage horizontal and vertical directions by setting the a parameter
+            // to use in svg attributes
             x = orient === DIR.H ? 'x' : 'y',
             y = orient === DIR.H ? 'y' : 'x';
 
-        function gridBase(context) {
+        /**
+         * Generator function for one-dimensional grid
+         * @param {*} context - d3 selection or transition to use as the container
+         */
+        function gridBaseGenerator(context) {
             let values = getValues(),
-                position = (scale.bandwidth ? center : number)(scale.copy()),
+
+                // Get the appropriate function to position the lines, based on scale type
+                // Pass a duplicate scale to ensure position values are fixed until grid updated
+                position = (scale.bandwidth ? positionCenter : positionNumber)(scale.copy()),
+
+                // Set parameter to ensure correct line offset positions for inverted ranges 
                 k = range[range.length - 1] >= range[0]? 1 : -1,
+
+                // If passed a transition, convert to selection
                 selection = context.selection ? context.selection() : context,
+
+                // Set up container element
                 initContainer = selection.selectAll(classArr.asSelector()).data([null]),
                 container = initContainer.merge(
                    initContainer.enter().append('g').attr('class', classArr.asList())
                 ),
+
+                // Set up line selections
                 line = container.selectAll('line').data(values, scale).order(),
                 lineExit = line.exit(),
                 lineEnter = line.enter().append('line').attr('stroke', '#000');
 
             line = line.merge(lineEnter);
 
+            // Run animations only if grid was called on a transition
             if (context !== selection) {
                 line = line.transition(context);
 
@@ -65,8 +103,8 @@ define(function(require) {
 
                 lineEnter = lineEnter
                     .attr('opacity', EPSILON)
-                    .attr(y + '1', function(d) { let p = this.parentNode.__grid; return p && isFinite(p = p(d)) ? p : position(d); })
-                    .attr(y + '2', function(d) { let p = this.parentNode.__grid; return p && isFinite(p = p(d)) ? p : position(d); });
+                    .attr(y + '1', function(d) { let p = this.parentNode.__pos; return p && isFinite(p = p(d)) ? p : position(d); })
+                    .attr(y + '2', function(d) { let p = this.parentNode.__pos; return p && isFinite(p = p(d)) ? p : position(d); });
             }
 
             lineExit.remove();
@@ -78,14 +116,20 @@ define(function(require) {
                 .attr(y + '1', d => position(d))
                 .attr(y + '2', d => position(d));
 
-            // Attach the positioning function to the container 
-            // so it can be referenced in future animations
-            // cannot use arrow function as this must be bound correctly
-            container.each(function() { this.__grid = position; });
+            // Attach the positioning function as a property of the container element 
+            // This stores it for future use as the starting point for the lineEnter transition
+            // Cannot use arrow function as this must refer to the element
+            container.each(function() {
+                this.__pos = position;
+            });
         }
 
         // HELPERS
 
+        /**
+         * Extract the tick values and adjust for edge hiding
+         * @private
+         */
         function getValues() {
             let hideFirst = hideEdges === true || hideEdges === 'both' || hideEdges === 'first',
                 hideLast = hideEdges === true || hideEdges === 'both' || hideEdges === 'last',
@@ -97,52 +141,147 @@ define(function(require) {
             return values;
         }
 
+        /**
+         * Get the tick values from the underlying scales
+         * @private
+         */
         function scaleTicks() {
-            return (scale.ticks 
-                ? scale.ticks.apply(scale, ticks ? [ticks] : []) 
-                : scale.domain()
-            ).slice();
+            let scaleTicks;
+
+            if (scale.ticks) {
+                scaleTicks = scale.ticks.apply(scale, ticks ? [ticks] : []);
+            } else {
+                scaleTicks = scale.domain();
+            }
+
+            return scaleTicks.slice();
         }
 
         // API
 
-        gridBase.scale = function(_) {
-            return arguments.length ? (scale = _, gridBase) : scale;
+        /**
+         * Gets or sets the scale
+         * Scale applies the ticks to the grid
+         * @param {*} [_] - d3 scale instance
+         * @return {*|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.scale = function(_) {
+            if (!arguments.length) {
+                return scale;
+            }
+            scale = _;
+
+            return gridBaseGenerator;
         };
 
-        gridBase.range = function(_) {
-            return arguments.length ? (range = _, gridBase) : range;
+        /**
+         * Gets or sets the range
+         * Governs the underlying length and positioning of the grid lines relative to the container
+         * Should usually be set to the output range from the orthogonal scale in a 2D chart
+         * @param {number[]} [_] - Array representing the output range
+         * @return {*|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.range = function(_) {
+            if (!arguments.length) {
+                return range;
+            }
+            range = _;
+
+            return gridBaseGenerator;
         }
 
-        gridBase.offsetStart = function(_) {
-            return arguments.length ? (offsetStart = _, gridBase) : offsetStart;
+        /**
+         * Gets or sets the start offset
+         * Start offset is the distance before the start position of the scale's range that the grid will render
+         * @param {number} [_] - Offset in px
+         * @return {*|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.offsetStart = function(_) {
+            if (!arguments.length) {
+                return offsetStart;
+            }
+            offsetStart = _;
+
+            return gridBaseGenerator;
         }
     
-        gridBase.offsetEnd = function(_) {
-            return arguments.length ? (offsetEnd = _, gridBase) : offsetEnd;
+        /**
+         * Gets or sets the end offset
+         * End offset is the distance after the end position of the scale's range that the grid will render
+         * @param {number} [_] - Offset in px
+         * @return {*|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.offsetEnd = function(_) {
+            if (!arguments.length) {
+                return offsetEnd;
+            }
+            offsetEnd = _;
+
+            return gridBaseGenerator;
         }
 
-        gridBase.hideEdges = function(_) {
-            return arguments.length ? (hideEdges = _, gridBase) : hideEdges;
+        /**
+         * Gets or sets the hideEdges parameter
+         * Determines if the first and last grid line are suppressed
+         * True or 'both' suppress both edges, 'first' and 'last' suppress the grid line
+         * corresponding to the first and last tick value respectively
+         * @param {boolean|string} [_] - hideEdges parameter, accepts boolean and 'both', 'first', or 'last' strings
+         * @return {boolean|string|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.hideEdges = function(_) {
+            if (!arguments.length) {
+                return hideEdges;
+            }
+            hideEdges = _;
+
+            return gridBaseGenerator;
         }
 
-        gridBase.ticks = function(_) {
-            return arguments.length ? (ticks = _, gridBase) : ticks;
-        };
-      
-        gridBase.tickValues = function(_) {
-            return arguments.length ? (tickValues = _ == null ? null : [..._].slice(), gridBase) : tickValues && tickValues.slice();
+        /**
+         * Gets or sets the tick count
+         * Mirrors d3 axis' ticks API method
+         * @param {number} [_] - Approximate tick count
+         * @return {number|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.ticks = function(_) {
+            if (!arguments.length) {
+                return ticks;
+            }
+            ticks = _;
+
+            return gridBaseGenerator;
         };
 
-        return gridBase;
+        /**
+         * Gets or sets the tick values
+         * Mirrors d3 axis' tickValues API method
+         * @param {number[]} [_] - Array of domain values to place ticks
+         * @return {number[]|gridBaseGenerator}
+         * @public
+         */
+        gridBaseGenerator.tickValues = function(_) {
+            if (!arguments.length) {
+                return tickValues && tickValues.slice();
+            }
+            tickValues = _ === null ? null : [..._].slice()
+
+            return gridBaseGenerator;
+        };
+
+        return gridBaseGenerator;
     }
 
     /**
-     * Constructor for a reusable two-dimensional grid helper
+     * Constructor for a two-dimensional grid helper
      * @param {*} scaleX - d3 scale for the grid's x direction
      * @param {*} scaleY - d3 scale for the grid's y direction
-     * @return {grid}
-     * @todo Fix this docblock
+     * @return {gridGenerator}
      */
     function grid(scaleX, scaleY) {
         let gridH = gridHorizontal(scaleY || scaleLinear()),
@@ -153,9 +292,9 @@ define(function(require) {
             
         /**
          * Generator function for two-dimensional grid
-         * @param {selection|transition} context - d3 selection or transition to use as the container
+         * @param {*} context - d3 selection or transition to use as the container
          */
-        function grid(context) {
+        function gridGenerator(context) {
             (direction === 'full' || direction === 'horizontal')
                 ? gridH.tickValues(tickValuesY).range(scaleX.range())
                 : gridH.tickValues([]);
@@ -168,28 +307,56 @@ define(function(require) {
         }
 
         // API
-        // TODO: Determine API handling for shared H and V attributes
-        //       ticks and tickValues definitely need to be independently settable
-        // TODO: Finalizing naming convention: V & H or X & Y for these accessors
 
-        grid.scaleX = function(_) {
-            return arguments.length ? (scaleX = _, gridV.scale(scaleX), grid) : scaleX;
+        /**
+         * Gets or sets the x-scale
+         * X-scale applies ticks to the vertical grid and range to the horizontal grid
+         * @param {*} [_] - d3 scale instance
+         * @return {*|gridGenerator}
+         * @public
+         */
+        gridGenerator.scaleX = function(_) {
+            if (!arguments.length) {
+                return scaleX;
+            }
+            scaleX = _;
+            gridV.scale(_);
+
+            return gridGenerator;
         }
     
-        grid.scaleY = function(_) {
-            return arguments.length ? (scaleY = _, gridH.scale(scaleY), grid) : scaleY;
+        /**
+         * Gets or sets the y-scale
+         * Y-scale applies ticks to the horizontal grid and range to the vertical grid
+         * @param {*} [_] - d3 scale instance
+         * @return {*|gridGenerator}
+         * @public
+         */
+        gridGenerator.scaleY = function(_) {
+            if (!arguments.length) {
+                return scaleY;
+            }
+            scaleY = _;
+            gridH.scale(_);
+
+            return gridGenerator;
         }
 
         /**
          * Gets or sets the direction of the grid
-         * Direction of full will render both horizontal and vertical grid lines
-         * Either horizontal or vertical wil render the respective lines
-         * @param {string} [_] - Grid direction of full, vertical, or horizontal
-         * @return {string|grid}
+         * Direction of 'full' will render both horizontal and vertical grid lines
+         * Either 'horizontal' or 'vertical' wil render the respective lines
+         * @param {string} [_] - Grid direction accepts 'full', 'vertical', or 'horizontal'
+         * @return {string|gridGenerator}
          * @public
          */
-        grid.direction = function(_) {
-            return arguments.length ? (direction = _, grid) : direction;
+        gridGenerator.direction = function(_) {
+            if (!arguments.length) {
+                return direction;
+            }
+            direction = _;
+
+            return gridGenerator;
         }
 
         /**
@@ -198,11 +365,17 @@ define(function(require) {
          * Returns the start offset of the horizontal grid if no argument is supplied
          * Start offset is the distance before the start position of the scale's range that the grid will render
          * @param {number} [_] - Offset in px
-         * @return {number|grid}
+         * @return {number|gridGenerator}
          * @public
          */
-        grid.offsetStart = function(_) {
-            return arguments.length ? (gridH.offsetStart(_), gridV.offsetStart(_), grid) : gridH.offsetStart();
+        gridGenerator.offsetStart = function(_) {
+            if (!arguments.length) {
+                return gridH.offsetStart();
+            }
+            gridH.offsetStart(_);
+            gridV.offsetStart(_);
+
+            return gridGenerator;
         }
     
         /**
@@ -211,11 +384,17 @@ define(function(require) {
          * Returns the end offset of the horizontal grid if no argument is supplied
          * End offset is the distance after the end position of the scale's range that the grid will render
          * @param {number} [_] - Offset in px
-         * @return {number|grid}
+         * @return {number|gridGenerator}
          * @public
          */
-        grid.offsetEnd = function(_) {
-            return arguments.length ? (gridH.offsetEnd(_), gridV.offsetEnd(_), grid) : gridH.offsetEnd();
+        gridGenerator.offsetEnd = function(_) {
+            if (!arguments.length) {
+                return gridH.offsetEnd();
+            }
+            gridH.offsetEnd(_);
+            gridV.offsetEnd(_);
+
+            return gridGenerator;
         }
 
         /**
@@ -224,12 +403,18 @@ define(function(require) {
          * Determines if the first and last grid line are suppressed
          * True or 'both' suppress both edges, 'first' and 'last' suppress the grid line
          * corresponding to the first and last tick value respectively
-         * @param {boolean|string} [_] - The hideEdges parameter
-         * @return {boolean|string|grid}
+         * @param {boolean|string} [_] - hideEdges parameter, accepts boolean and 'both', 'first', or 'last' strings
+         * @return {boolean|string|gridGenerator}
          * @public
          */
-        grid.hideEdges = function(_) {
-            return arguments.length ? (gridH.hideEdges(_), gridV.hideEdges(_), grid) : gridH.hideEdges();
+        gridGenerator.hideEdges = function(_) {
+            if (!arguments.length) {
+                return gridH.hideEdges();
+            }
+            gridH.hideEdges(_);
+            gridV.hideEdges(_);
+
+            return gridGenerator;
         }
 
         /**
@@ -237,32 +422,53 @@ define(function(require) {
          * Returns the horizontal ticks if no argument specified
          * Mirrors d3 axis' ticks API method
          * @param {number} [_] - Approximate tick count
-         * @return {number|grid}
+         * @return {number|gridGenerator}
          * @public
          */
-        grid.ticks = function(_) {
-            return arguments.length ? (gridH.ticks(_), gridV.ticks(_), grid) : gridH.ticks();
+        gridGenerator.ticks = function(_) {
+            if (!arguments.length) {
+                return gridH.ticks();
+            }
+            gridH.ticks(_);
+            gridV.ticks(_);
+
+            return gridGenerator;
         }
 
         /**
-         * Gets or sets the tick value for both horizontal and vertical grids
+         * Gets or sets the tick values for both horizontal and vertical grids
          * Returns the horizontal tick values if no argument specified
          * Mirrors d3 axis' tickValues API method
          * @param {number[]} [_] - Array of domain values to place ticks
-         * @return {number[]|grid}
+         * @return {number[]|gridGenerator}
          * @public
          */
-        grid.tickValues = function(_) {
-            return arguments.length ? (tickValuesX = tickValuesY = _, grid) : tickValuesY;
+        gridGenerator.tickValues = function(_) {
+            if (!arguments.length) {
+                return tickValuesY; 
+            }
+            tickValuesX = tickValuesY = _;
+
+            return gridGenerator;
         }
 
-        return grid;
+        return gridGenerator;
     }
 
+    /**
+     * Constructor for a horizontal grid helper
+     * @param {*} scale - d3 scale to initialize the grid
+     * @return {gridBaseGenerator}
+     */
     function gridHorizontal(scale) {
-        return gridBase(DIR.H, scale)
+        return gridBase(DIR.H, scale);
     }
 
+    /**
+     * Constructor for a vertical grid helper
+     * @param {*} scale - d3 scale to initialize the grid
+     * @return {gridBaseGenerator}
+     */
     function gridVertical(scale) {
         return gridBase(DIR.V, scale);
     }
