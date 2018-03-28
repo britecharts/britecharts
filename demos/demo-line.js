076506e7bca16279a0bc8319ba857756e649125b
Daler Asrorov <daler.asrorov@eventbrite.com>
2018-03-28 00:21:42 -0700

    Reset line demo

--- demos/demo-line.js
+++ demos/demo-line.js
@@ -66,7 +66,7 @@ function createLineChart(optionalColorSchema, optionalData) {
         // LineChart Setup and start
         lineChart1
             .isAnimated(true)
-            .aspectRatio(0.4)
+            .aspectRatio(0.5)
             .grid('horizontal')
             .tooltipThreshold(600)
             .width(containerWidth)
