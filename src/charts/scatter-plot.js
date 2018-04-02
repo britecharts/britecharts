bff8475ce55f8d7475e786926263e95fc72c9afd
Daler Asrorov <daler.asrorov@eventbrite.com>
2018-04-01 23:05:34 -0700

    return margin.left and mergin.right adjustments

--- src/charts/scatter-plot.js
+++ src/charts/scatter-plot.js
@@ -532,6 +532,11 @@ define(function(require) {
                       .attr('cy', (d) => yScale(d.y))
                       .style('cursor', 'pointer');
             }
+
+            svg.selectAll('.metadata-group').selectAll("path")
+                .data(voronoi.polygons())
+                .enter().append("path")
+                .attr("d", function (d) { return d ? "M" + d.join("L") + "Z" : null; })
         }
 
         /**
@@ -542,6 +547,8 @@ define(function(require) {
          */
         function getPointProps(svg) {
             let mousePos = d3Selection.mouse(svg);
+            mousePos[0] -= margin.left;
+            mousePos[1] -= margin.top;
 
             return {
                 closestPoint: voronoi.find(mousePos[0], mousePos[1]),
