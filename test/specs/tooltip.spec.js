71ba215ecdeffa86cd988fa1ca855b88f6dde98b
Adrián de la Rosa <adriantovick@gmail.com>
2018-04-11 12:27:56 +0200

    valueFormatter setter and getter in tooltip and mini-tooltip (#598)
    * valueFormatter setter and getter in tooltip and mini-tooltip
    
    * jsdoc examples added to valueFormatter getter/setter in tooltip and mini-tooltip
    
    * return should have an empty line before

--- test/specs/tooltip.spec.js
+++ test/specs/tooltip.spec.js
@@ -390,6 +390,29 @@ define(['jquery', 'd3', 'tooltip'], function($, d3, tooltip) {
 
                     expect(actual).toEqual(expected);
                 });
+
+                it('should use custom function if set', () => {
+                    let expected = '8',
+                        actual;
+
+                    tooltipChart.valueFormatter(value => value.toString().length.toString());
+
+                    tooltipChart.update({
+                        date: '2015-08-05T07:00:00.000Z',
+                        topics: [
+                            {
+                                name: 103,
+                                value: 10000000,
+                                topicName: 'San Francisco'
+                            }
+                        ]
+                    }, topicColorMap, 0);
+
+                    actual = containerFixture.select('.britechart-tooltip .tooltip-right-text')
+                                .text();
+
+                    expect(actual).toEqual(expected);
+                });
             });
         });
 
