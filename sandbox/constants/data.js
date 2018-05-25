121efa4cc17998540ad76d80be00c820a7408da3
Daler Asrorov <asrorids@gmail.com>
2018-05-26 00:06:46 +0300

    Docs - added Scatter plot to sandbox (#619)

--- sandbox/constants/data.js
+++ sandbox/constants/data.js
@@ -138,6 +138,22 @@ module.exports = (function() {
                 { date: '2017-02-20T00:00:00', name: 'Organizer Driven', value: 18 },
                 { date: '2017-02-20T00:00:00', name: 'EB Driven', value: 1 }
             ]
+        },
+        'scatter-plot': {
+            basicScatterPlot: [
+                { name: 'Ice Cream Sales', x: 14.2, y: 215 },
+                { name: 'Ice Cream Sales', x: 16.4, y: 325 },
+                { name: 'Ice Cream Sales', x: 11.9, y: 185 },
+                { name: 'Ice Cream Sales', x: 15.2, y: 332 },
+                { name: 'Ice Cream Sales', x: 18.5, y: 406 },
+                { name: 'Ice Cream Sales', x: 22.1, y: 522 },
+                { name: 'Ice Cream Sales', x: 19.4, y: 412 },
+                { name: 'Ice Cream Sales', x: 25.1, y: 614 },
+                { name: 'Ice Cream Sales', x: 23.4, y: 544 },
+                { name: 'Ice Cream Sales', x: 18.1, y: 421 },
+                { name: 'Ice Cream Sales', x: 22.6, y: 445 },
+                { name: 'Ice Cream Sales', x: 17.2, y: 408 }
+            ]
         }
     };
 }());
