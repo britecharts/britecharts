e823857fb4c95e0ba196914256c6ce2294e3addc
Marcos Iglesias <golodhros@gmail.com>
2019-05-14 00:30:05 +0100

    Update Britecharts Node to 12.2.0 (#715)
    * Updating node to 12, dependencies working
    
    * Updating node versions in docker and travis configs
    
    * Updating docs with Docker setup
    
    * Dots on Karma reporter

--- src/tasks/cssmin.js
+++ src/tasks/cssmin.js
@@ -1,33 +1,33 @@
 module.exports = function (grunt, configOptions) {
     'use strict';
 
-    var distFiles = [
-            // Main bundle with all the styles on the library
-            {
-                expand: true,
-                cwd: './dist/css',
-                src: ['britecharts.css', '!britecharts.min.css'],
-                dest: './dist/css',
-                ext: '.min.css'
-            },
-            // Common bundle including axes and gridlines
-            // specified on common.scss
-            {
-                expand: true,
-                cwd: './dist/css/common',
-                src: ['common.css', '!common.min.css'],
-                dest: './dist/css/common/',
-                ext: '.min.css'
-            },
-            // Individual styles for each chart
-            {
-                expand: true,
-                cwd: './dist/css/charts',
-                src: ['*.css', '!*.min.css'],
-                dest: './dist/css/charts',
-                ext: '.min.css'
-            }
-        ];
+    const distFiles = [
+        // Main bundle with all the styles on the library
+        {
+            expand: true,
+            cwd: './dist/css',
+            src: ['britecharts.css', '!britecharts.min.css'],
+            dest: './dist/css',
+            ext: '.min.css'
+        },
+        // Common bundle including axes and gridlines
+        // specified on common.scss
+        {
+            expand: true,
+            cwd: './dist/css/common',
+            src: ['common.css', '!common.min.css'],
+            dest: './dist/css/common/',
+            ext: '.min.css'
+        },
+        // Individual styles for each chart
+        {
+            expand: true,
+            cwd: './dist/css/charts',
+            src: ['*.css', '!*.min.css'],
+            dest: './dist/css/charts',
+            ext: '.min.css'
+        }
+    ];
 
     grunt.config.set('cssmin', {
         dist: {
