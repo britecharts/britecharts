7a9300e4deab54aa95e79a6ad96522ce38ef6080
Marcos Iglesias <golodhros@gmail.com>
2019-05-14 00:30:05 +0100

    Update Britecharts Node to 12.2.0 (#715)
    * Updating node to 12, dependencies working
    
    * Updating node versions in docker and travis configs
    
    * Updating docs with Docker setup
    
    * Dots on Karma reporter

--- src/tasks/sass.js
+++ src/tasks/sass.js
@@ -1,52 +1,56 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var distFiles = [
-            // Main bundle with all the styles on the library
-            {
-                expand: true,
-                cwd: 'src/styles/',
-                src: ['britecharts.scss'],
-                dest: './dist/css',
-                ext: '.css'
-            },
-            // Common bundle including axes and gridlines
-            // specified on common.scss
-            {
-                expand: true,
-                cwd: 'src/styles/',
-                src: ['common.scss'],
-                dest: './dist/css/common/',
-                ext: '.css'
-            },
-            // Individual styles for each chart
-            {
-                expand: true,
-                cwd: 'src/styles/charts',
-                src: ['*.scss'],
-                dest: './dist/css/charts',
-                ext: '.css'
-            }
-        ],
-        devFiles = [
-            {
-                expand: true,
-                cwd: 'src/styles/',
-                src: ['britecharts.scss'],
-                dest: './demos/css',
-                ext: '.css'
-            }
-        ];
+    const sass = require('node-sass');
+
+    const distFiles = [
+        // Main bundle with all the styles on the library
+        {
+            expand: true,
+            cwd: 'src/styles/',
+            src: ['britecharts.scss'],
+            dest: './dist/css',
+            ext: '.css'
+        },
+        // Common bundle including axes and gridlines
+        // specified on common.scss
+        {
+            expand: true,
+            cwd: 'src/styles/',
+            src: ['common.scss'],
+            dest: './dist/css/common/',
+            ext: '.css'
+        },
+        // Individual styles for each chart
+        {
+            expand: true,
+            cwd: 'src/styles/charts',
+            src: ['*.scss'],
+            dest: './dist/css/charts',
+            ext: '.css'
+        }
+    ];
+    const devFiles = [
+        {
+            expand: true,
+            cwd: 'src/styles/',
+            src: ['britecharts.scss'],
+            dest: './demos/css',
+            ext: '.css'
+        }
+    ];
 
     grunt.config.set('sass', {
         dev: {
             options: {
+                implementation: sass,
                 style: 'expanded'
             },
             files: devFiles
         },
         dist: {
             options: {
+                implementation: sass,
                 style: 'expanded'
             },
             files: distFiles
