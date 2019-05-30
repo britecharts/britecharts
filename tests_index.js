daf5553ae1ccc55dd90b38aedea1361c0af8eb2e
Marcos Iglesias <golodhros@gmail.com>
2019-05-30 15:26:48 -0700

    Updates code coverage configuration (#726)

--- tests_index.js
+++ tests_index.js
@@ -1,6 +1,6 @@
-var testsContext = require.context('./test/specs', true, /spec\.js$/),
-    srcContext = require.context('./src/charts', true, /\.js$/),
-    jsFixturesContext = require.context('./test/fixtures', true, /\.js$/);
+const testsContext = require.context('./test/specs', true, /spec\.js$/);
+const srcContext = require.context('./src/charts', true, /\.js$/);
+const jsFixturesContext = require.context('./test/fixtures', true, /\.js$/);
 
 testsContext.keys().forEach(testsContext);
 srcContext.keys().forEach(srcContext);
