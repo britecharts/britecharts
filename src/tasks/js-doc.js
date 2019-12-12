8e66ef7e34f19b26893a43d15ba54ccb63ece347
Marcos Iglesias <golodhros@gmail.com>
2019-12-11 22:19:51 -0800

    Adds CSS and JavaScript linting plus code formatting
    Fixes bar chart issue and adds test
    Removes coverage summary
    Adds prettier formatting and first run, removes coverage summary
    Updates eslint cache
    CSS linting and first run
    Adds Sass guidelines and fixes issues

--- src/tasks/js-doc.js
+++ src/tasks/js-doc.js
@@ -1,23 +1,25 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     'use strict';
 
     // Project configuration.
     grunt.config.set('jsdoc', {
-        dist : {
-            src: ['./src/charts/*.js','./README.md'],
+        dist: {
+            src: ['./src/charts/*.js', './README.md'],
             jsdoc: './node_modules/.bin/jsdoc',
             options: {
                 destination: 'docs',
                 configure: 'src/doc/jsdoc.conf.json',
                 template: 'src/doc/template',
                 tutorials: 'demos',
-                mainpagetitle:'Britecharts - D3.js based charting library of reusable components'
-            }
-        }
+                mainpagetitle:
+                    'Britecharts - D3.js based charting library of reusable components',
+            },
+        },
     });
 
-    grunt.registerTask('docs', 'Generates docs and triggers server to view them', [
-        'jsdoc:dist',
-        'sync'
-    ]);
+    grunt.registerTask(
+        'docs',
+        'Generates docs and triggers server to view them',
+        ['jsdoc:dist', 'sync']
+    );
 };
--- src/tasks/test.js
+++ src/tasks/test.js
@@ -1,15 +1,15 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     'use strict';
 
     // grunt-karma configuration.
     grunt.config.set('karma', {
         unit: {
             configFile: 'karma.conf.js',
-            autoWatch: true
-        }
+            autoWatch: true,
+        },
     });
 
     grunt.registerTask('test', 'Run the unit tests of the charts', [
-        'karma:unit'
+        'karma:unit',
     ]);
 };
