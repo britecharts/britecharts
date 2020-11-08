44369286597ff0a01fbc7d3e743ebab66101b594
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-11-08 09:23:31 -0800

    feat: Adds types to Britecharts via declaration files (#878)
    * Adds types from our types project
    
    * fix: typings index export resolution
    
    - Adds @typescript-eslint/parser as dependency
    - Adds override for eslint just for d.ts files.
    
    Dependencies has been automaticaly sort on package.json by yarn.
    
    * Fixing types, working on test project
    
    * Removing unnecessary imports
    
    Co-authored-by: Marcos Gomez Castillo <adayintexas@gmail.com>

---
+++ src/typings/common/grid.d.ts
@@ -0,0 +1,1 @@
+export type GridTypes = 'vertical' | 'horizontal' | 'full';
---
+++ src/typings/common/margin.d.ts
@@ -0,0 +1,6 @@
+export interface ChartMarginParams {
+  left?: number;
+  right?: number;
+  top?: number;
+  bottom?: number;
+}
