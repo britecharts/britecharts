233b0af1996b9db2975f90a056cff7f5a382f593
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-11-08 09:23:31 -0800

    feat: Adds types to Britecharts via declaration files (#878)
    * Adds types from our types project
    
    * fix: typings index export resolution
    
    - Adds @typescript-eslint/parser as dependency
    - Adds override for eslint just for d.ts files.
    
    Dependencies has been automaticaly sort on package.json by yarn.
    
    * Fixing types, working on test project
    
    * Removing unnecessary imports
    
    Co-authored-by: Marcos Gomez Castillo <adayintexas@gmail.com>

---
+++ src/typings/common/local.d.ts
@@ -0,0 +1,10 @@
+export type LocalObject = {
+  decimal?: string;
+  thousands?: string;
+  grouping?: number[];
+  currency?: string[];
+  numerals?: string[];
+  percent?: string;
+  minus?: string;
+  nan?: string;
+};
