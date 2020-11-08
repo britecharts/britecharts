89e2f3f98d5dfafaffec550e9987d12216d527c9
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-11-08 09:23:31 -0800

    feat: Adds types to Britecharts via declaration files (#878)
    * Adds types from our types project
    
    * fix: typings index export resolution
    
    - Adds @typescript-eslint/parser as dependency
    - Adds override for eslint just for d.ts files.
    
    Dependencies has been automaticaly sort on package.json by yarn.
    
    * Fixing types, working on test project
    
    * Removing unnecessary imports
    
    Co-authored-by: Marcos Gomez Castillo <adayintexas@gmail.com>

---
+++ src/typings/charts/heatmap-chart.d.ts
@@ -0,0 +1,30 @@
+import { BaseAPI, ExportableChartAPI } from '../common/base';
+import { ChartModuleSelection } from '../common/selection';
+
+export enum HeatmapChartKeys {
+  Day = 'day',
+  Hour = 'hour',
+  Value = 'value',
+}
+
+export type HeatmapChartDataShape = {
+  [HeatmapChartKeys.Day]: number;
+  [HeatmapChartKeys.Hour]: number;
+  [HeatmapChartKeys.Value]: number;
+};
+
+export interface HeatmapChartAPI extends BaseAPI<HeatmapChartModule>, ExportableChartAPI<HeatmapChartModule>  {
+  /** Gets or Sets the y-axis labels of the chart */
+  yAxisLabels(labels?: string[]): HeatmapChartModule;
+  /** Gets or Sets the boxSize of the chart */
+  boxSize(size?: number): HeatmapChartModule;
+}
+
+export type HeatmapChartModule = ChartModuleSelection<HeatmapChartDataShape[]> &
+  HeatmapChartAPI;
+
+/**
+ * import {heatmap} from 'britecharts;
+ * heatmap().width(100).height(100)
+ */
+export function heatmap(): HeatmapChartModule;
