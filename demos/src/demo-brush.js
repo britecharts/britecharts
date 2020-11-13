055fbe70f8176d8931c80040c695d7e21dc064df
Marcos Iglesias <miglesiasvalle@lyft.com>
2020-11-13 10:44:28 -0800

    Second Brush example

--- demos/src/demo-brush.js
+++ demos/src/demo-brush.js
@@ -49,19 +49,68 @@ function createBrushChart() {
     return brushChart;
 }
 
+function createOtherBrushChart() {
+    const brushChart = brush();
+    const testDataSet = new BrushDataBuilder();
+    const brushContainer = select('.js-other-brush-chart-container');
+    const containerWidth = brushContainer.node()
+        ? brushContainer.node().getBoundingClientRect().width
+        : false;
+    let dataset;
+
+    let elementDateRange = select('.js-other-date-range');
+
+    if (containerWidth) {
+        dataset = testDataSet.withShortData().build();
+
+        brushChart
+            .width(containerWidth)
+            .height(125)
+            .on('customBrushStart', function (brushExtent) {
+                let format = timeFormat('%m/%d/%Y');
+
+                select('.js-other-start-date').text(format(brushExtent[0]));
+                select('.js-other-end-date').text(format(brushExtent[1]));
+
+                elementDateRange.classed('is-hidden', false);
+            })
+            .on('customBrushEnd', function (brushExtent) {
+                // eslint-disable-next-line no-console
+                console.log('rounded extent', brushExtent);
+
+                if (brushExtent[0] === null) {
+                    elementDateRange.classed('is-hidden', true);
+                }
+            });
+
+        brushContainer.datum(dataset).call(brushChart);
+
+        brushChart.dateRange(['1/06/2011', '1/07/2011']);
+    }
+
+    return brushChart;
+}
+
 // Show charts if container available
 if (select('.js-brush-chart-container').node()) {
-    let brushChart = createBrushChart();
+    const brushChart = createBrushChart();
+    const otherBrushChart = createOtherBrushChart();
 
     const redrawCharts = function () {
         const brushContainer = select('.js-brush-chart-container');
         const containerWidth = brushContainer.node()
             ? brushContainer.node().getBoundingClientRect().width
             : false;
+        const otherBrushContainer = select('.js-other-brush-chart-container');
+        const otherContainerWidth = otherBrushContainer.node()
+            ? otherBrushContainer.node().getBoundingClientRect().width
+            : false;
 
         brushChart.width(containerWidth).dateRange([null, null]);
+        otherBrushChart.width(otherContainerWidth).dateRange([null, null]);
 
         brushContainer.call(brushChart);
+        otherBrushContainer.call(otherBrushChart);
     };
 
     // Redraw charts on window resize
@@ -69,6 +118,10 @@ if (select('.js-brush-chart-container').node()) {
 
     select('#clear-selection').on('click', function (e) {
         brushChart.dateRange([null, null]);
+        event.preventDefault();
+    });
+    select('#other-clear-selection').on('click', function (e) {
+        otherBrushChart.dateRange([null, null]);
         event.preventDefault();
     });
 }
