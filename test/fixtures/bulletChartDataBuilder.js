724cd54ee652e85f72b1f9074ed43849696b8b8d
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-12-04 20:44:18 -0800

    Fixes issue where bullet won't get the right range (#895)

--- test/fixtures/bulletChartDataBuilder.js
+++ test/fixtures/bulletChartDataBuilder.js
@@ -35,6 +35,50 @@ define(function(require) {
             return new this.Klass(attributes);
         };
 
+        this.withManyCPUData = function () {
+            var attributes = _.extend({}, this.config, {
+                data: [
+                    {
+                        title: 'CPU 1',
+                        subtitle: 'GHz',
+                        ranges: [1500, 2100, 3500],
+                        measures: [1800, 2200],
+                        markers: [2600, 3000],
+                    },
+                    {
+                        title: 'CPU 2',
+                        subtitle: 'GHz',
+                        ranges: [1300, 1900, 3000],
+                        measures: [1700, 2000],
+                        markers: [1600],
+                    },
+                    {
+                        title: 'CPU 3',
+                        subtitle: 'GHz',
+                        ranges: [1300, 1500, 2000],
+                        measures: [1500, 1800],
+                        markers: [1950],
+                    },
+                    {
+                        title: 'CPU 4',
+                        subtitle: 'GHz',
+                        ranges: [1000, 2000],
+                        measures: [1500],
+                        markers: [],
+                    },
+                    {
+                        title: 'CPU 5',
+                        subtitle: 'GHz',
+                        ranges: [1000, 2000],
+                        measures: [500],
+                        markers: [],
+                    }
+                ],
+            });
+
+            return new this.Klass(attributes);
+        };
+
         this.build = function() {
             return this.config.data;
         };
