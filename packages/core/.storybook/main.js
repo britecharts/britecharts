f5143a1e8d16d622d3985cbd02de6461aae5be55
Marcos Iglesias <golodhros@gmail.com>
2022-03-05 11:15:01 -0800

    feat: adds storybook in core package, WIP due to babel issues?

---
+++ packages/core/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+    stories: ['../src/**/*.stories.[tj]s'],
+    addons: [
+        '@storybook/addon-postcss',
+        '@storybook/addon-viewport/register',
+        '@storybook/addon-a11y',
+        '@storybook/addon-actions',
+    ],
+};
