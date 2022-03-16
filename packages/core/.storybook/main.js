7bfb14efe3a2f422124c17470c3b45cfa837ee78
Marcos Iglesias <golodhros@gmail.com>
2022-03-15 17:29:24 -0700

    chore: cleanup and storybook bc styles loaded

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
     stories: ['../src/**/*.stories.[tj]s'],
     addons: [
@@ -5,4 +7,19 @@ module.exports = {
         '@storybook/addon-a11y',
         '@storybook/addon-actions',
     ],
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
+
+        // Make whatever fine-grained changes you need
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            include: path.resolve(__dirname, '../'),
+        });
+
+        // Return the altered config
+        return config;
+    },
 };
