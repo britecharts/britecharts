b8ec8a2b899e76d2e22daf7a84dfd0e1f96156c0
Marcos Iglesias <golodhros@gmail.com>
2022-03-29 18:25:40 -0700

    feat: legend demos

---
+++ packages/react/src/charts/legend/legend.stories.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Legend from './Legend';
+import legendData from './legendChart.fixtures';
+import { colors } from '@britecharts/core';
+
+export default {
+    title: 'Charts/Legend',
+    component: Legend,
+};
+
+export const WithDefaultProperties = () => {
+    const data = legendData.with6Points();
+
+    return <Legend data={data} height={250} width={400} />;
+};
+
+export const WithHorizontalDirection = () => {
+    const data = legendData.with6Points();
+
+    return (
+        <Legend
+            data={data}
+            isHorizontal={true}
+            marginRatio={1.8}
+            height={100}
+        />
+    );
+};
+
+export const WithHorizontalDirectionAndSmallerDots = () => {
+    const data = legendData.with6Points();
+
+    return (
+        <Legend
+            data={data}
+            isHorizontal={true}
+            markerSize={10}
+            marginRatio={1.8}
+            width={500}
+            height={100}
+        />
+    );
+};
