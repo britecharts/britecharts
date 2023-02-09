1ae3a61d27224768952f97d12fa3ff169f57d08d
Marcos Iglesias <golodhros@gmail.com>
2023-02-08 21:41:36 -0800

    chore: testing chromatic setup

--- packages/demos/.storybook/main.js
+++ packages/demos/.storybook/main.js
@@ -1,14 +1,30 @@
 module.exports = {
     stories: ['../src/*.stories.[tj]s'],
     addons: [],
-    refs: {
-        core: {
-            title: 'Britecharts Core',
-            url: 'http://localhost:1111',
-        },
-        react: {
-            title: 'Britecharts React',
-            url: 'http://localhost:2222',
-        },
+
+    // Reference: Storybook composition: https://storybook.js.org/docs/react/sharing/storybook-composition
+    refs: (_, { configType }) => {
+        if (configType === 'DEVELOPMENT') {
+            return {
+                core: {
+                    title: 'Britecharts Core',
+                    url: 'http://localhost:1111',
+                },
+                react: {
+                    title: 'Britecharts React',
+                    url: 'http://localhost:2222',
+                },
+            };
+        }
+        return {
+            core: {
+                title: 'Britecharts Core',
+                url: 'https://63e47b02f004ed290364764f-dmivwchyrh.chromatic.com',
+            },
+            react: {
+                title: 'Britecharts React',
+                url: 'http://localhost:2222',
+            },
+        };
     },
 };
